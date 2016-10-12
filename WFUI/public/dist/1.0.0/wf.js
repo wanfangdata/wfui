a7af194b31ceba63c33df580fc6e8a369c828aa5
biss <biss@wanfangdata.com.cn>
2016-10-12 14:54:32 +0800

    loader

--- WFUI/public/dist/1.0.0/wf.js
+++ WFUI/public/dist/1.0.0/wf.js
@@ -19,6 +19,9 @@
                  */
                 modules = {};
 
+            /**
+             * WFUI实例
+             */
             return {
                 
                 /**
@@ -330,40 +333,75 @@ wf.define('cookie', [], function () {
     };
 });
 'use strict';
+
 wf.define('loader', [], function () {
+
+    /**
+     * private
+     * 动态加载模块集合
+     */
+    var loadModules = {},
+
+        /**
+         * 创建模块node
+         * private
+         * @method createModuleNode
+         * @param {String} path 模块url
+         * @return {String} node
+         */
+        createModuleNode = function (path) {
+            var node = document.createElement('script');
+            node.type = 'text/javascript';
+            node.async = 'true';
+            node.src = path + '.js';
+            return node;
+        },
+
+        /**
+         * check所有模块加载完成执行callback
+         * @param {Function} callback 加载完成回调函数
+         * private
+         */
+        checkAllFiles = function (callback) {
+            var allLoaded = true;
+
+            for (var i = 0; i < pathArr.length; i++) {
+                if (!loadModules[pathArr[i]]) {
+                    allLoaded = false;
+                    break;
+                }
+            }
+
+            if (allLoaded) {
+                callback();
+            }
+        }
+
+    /**
+     * public api
+     */
     return {
+        /**
+         * 获取日志输出模式
+         * @method load 动态获取模块
+         */
         load: function (pathArr, callback) {
             for (var i = 0; i < pathArr.length; i++) {
+
                 var path = pathArr[i];
-                
-                if (!fileMap[path]) {
+
+                if (!loadModules[path]) {
                     var head = document.getElementsByTagName('head')[0];
-                    var node = document.createElement('script');
-                    node.type = 'text/javascript';
-                    node.async = 'true';
-                    node.src = path + '.js';
+                    var node = createModuleNode(path);
                     node.onload = function () {
-                        fileMap[path] = true;
+                        loadModules[path] = true;
                         head.removeChild(node);
-                        checkAllFiles();
+                        checkAllFiles(callback);
                     };
                     head.appendChild(node);
                 }
             }
-            
-            function checkAllFiles() {
-                var allLoaded = true;
-                for (var i = 0; i < pathArr.length; i++) {
-                    if (!fileMap[pathArr[i]]) {
-                        allLoaded = false;
-                        break;
-                    }
-                }
-                
-                if (allLoaded) {
-                    callback();
-                }
-            }
+
         }
     };
 });
