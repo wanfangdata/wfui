77e2b21b3e9927638c988a618d6e5e0fc92c0ae2
biss <biss@wanfangdata.com.cn>
2016-10-21 15:37:53 +0800

    jade模板引擎替换为vash

--- WFUI/controllers/home.js
+++ WFUI/controllers/home.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    var items = [];
+    for (var i = 0; i < 10; i++) {
+        
+        items.push({ nick: 'nick' + i, date: new Date(), args: ['zero', 'one'] });
+    }
+    res.render('index', {
+        channel: 'Express'
+        , buffer: items
+    });
 });
 
 module.exports = router;
