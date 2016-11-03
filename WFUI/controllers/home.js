bbc5f00c9662c75127c2e974d48f8458f205e86f
biss <biss@wanfangdata.com.cn>
2016-11-03 15:46:13 +0800

    menu data

--- WFUI/controllers/home.js
+++ WFUI/controllers/home.js
@@ -1,16 +1,13 @@
 ﻿var express = require('express');
+var menu = require('../data/menu');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
     var items = [];
-    for (var i = 0; i < 10; i++) {
-        
-        items.push({ nick: 'nick' + i, date: new Date(), args: ['zero', 'one'] });
-    }
+    
     res.render('index', {
-        channel: 'Express'
-        , buffer: items
+        menu: menu
     });
 });
 
---
+++ WFUI/data/menu.js
@@ -0,0 +1,70 @@
+﻿var menu = {
+    label: 'WFUI',
+    data: [
+        {
+            label: 'Get started',
+            name: '开始使用'            
+        }, {
+            label: 'Basic',
+            name: '基础组件',
+            data: [
+                {
+                    label: 'Layout',
+                    name: '栅格'
+                }, {
+                    label: 'Button',
+                    name: '按钮'
+                }, {
+                    label: 'Icon',
+                    name: '图标'
+                }
+            ]
+        }, {
+            label: 'Navigation',
+            name: '导航组件',
+            data: [
+                {
+                    label: 'Breadcrumb',
+                    name: '面包屑'
+                }, {
+                    label: 'Menu',
+                    name: '导航菜单'
+                }, {
+                    label: 'Tab',
+                    name: '标签页'
+                }, {
+                    label: 'Pagination',
+                    name: '分页'
+                }, {
+                    label: 'Step',
+                    name: '步骤条'
+                }
+            ]
+        }, {
+            label: 'Form control',
+            name: '表单组件',
+            data: [
+                {
+                    label: 'Checkbox',
+                    name: '多选框'
+                }, {
+                    label: 'Form',
+                    name: '表单'
+                }, {
+                    label: 'input',
+                    name: '输入框'
+                }, {
+                    label: 'Radio',
+                    name: '单选框'
+                }, {
+                    label: 'Select',
+                    name: '选择框'
+                }
+            ]
+        }, {
+            label: 'Popup',
+            name: '弹出层组件'
+        }
+    ]
+};
+module.exports = menu;
