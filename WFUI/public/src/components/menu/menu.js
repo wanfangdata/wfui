edb7504099337df81f531b22b52a2c028622e830
biss <biss@wanfangdata.com.cn>
2016-11-04 13:34:56 +0800

    修改注释及init内容

--- WFUI/public/src/components/menu/menu.js
+++ WFUI/public/src/components/menu/menu.js
@@ -7,7 +7,7 @@ wf.define('UI.Menu', ['UI', 'logger', 'Action'], function (UI, logger, Action) {
 
 
     /**
-     * @class Checkbox
+     * @class Menu
      */
     var Menu = wf.inherit(UI, {
 
@@ -18,23 +18,12 @@ wf.define('UI.Menu', ['UI', 'logger', 'Action'], function (UI, logger, Action) {
 
         /**
          * ui初始化
-         * @param {String} _base_ 父类同名方法
-         * @param {String} name ui名
-         * @param {Object} $element ui jquery对象
-         * @param {Bool} checked 是否选中
-         * @param {Object} events 组件事件
-         * events:{'click',function($element){}}
          */
-        init: function (_base_, name, $element, checked, events) {
-            _base_(name, $element, events);
-            //初始化组件元素,为JQuery对象
+        init: function (_base_, name, $element) {
+            _base_(name, $element);
             this.initElement([
-                this.inner,
-                this.input,
-                this.text
+
             ]);
-            //初始化选中状态
-            this.checked(checked || false);
             //初始化事件
             this.action = this.actionHandler();
             this.initEvent(events);
