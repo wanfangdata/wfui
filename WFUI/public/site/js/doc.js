afcc31597f7dbcd030358af12e6c3bf7fbce1e8c
biss <biss@wanfangdata.com.cn>
2016-12-07 16:14:03 +0800

    primary btn hover in group

--- WFUI/public/site/js/doc.js
+++ WFUI/public/site/js/doc.js
@@ -2,5 +2,11 @@ wf.require('page').render(
     'doc', ['UI.Checkbox'], function (components) {
         console.log(this);
         //$(document).pjax('.wf-menu-item a', '.doc-content');
+        $('.page .section .title .wf-btn').click(function () {
+            $(this).parent().parent().find('pre').toggle(200);
+        });
+        $('.page .wf-btn-toggleall').click(function () {
+            $(this).parent().parent().find('pre').toggle(200);
+        });
     },true
 );
