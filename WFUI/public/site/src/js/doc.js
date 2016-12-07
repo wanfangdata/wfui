4cc96cce2c2e75e074de290df2b8c6d92de39d42
biss <biss@wanfangdata.com.cn>
2016-12-07 16:57:25 +0800

    fixed nav

--- WFUI/public/site/src/js/doc.js
+++ WFUI/public/site/src/js/doc.js
@@ -2,11 +2,16 @@
     'doc', ['UI.Checkbox'], function (components) {
         console.log(this);
         //$(document).pjax('.wf-menu-item a', '.doc-content');
-        $('.page .section .title .wf-btn').click(function () {
+        $('.page .section .title .wf-btn,.page .wf-btn-toggleall').click(function () {
             $(this).parent().parent().find('pre').toggle(200);
         });
-        $('.page .wf-btn-toggleall').click(function () {
-            $(this).parent().parent().find('pre').toggle(200);
-        });
+        $('.doc-content nav').html((function () {
+            var result = [];
+            var temp = '<a href="#{0}">{1}</a>';
+            $('.doc-content .section').each(function (i,item) { 
+                result.push(temp.format($(this).attr('id'), $(this).find('h1.title .text').html()));
+            });
+            return result.join('');
+        })(this));
     },true
 );
