3d1f2cffa75aa6e6c3b09888e8c2ee0e2dd40b74
biss <biss@wanfangdata.com.cn>
2017-01-08 15:37:12 +0800

    add browser

--- WFUI/public/dist/1.0.0/wf.js
+++ WFUI/public/dist/1.0.0/wf.js
@@ -500,6 +500,41 @@ wf.define('loader', [], function () {
 });
 'use strict';
 /**
+ * browser core and version
+ */
+wf.define('Browser', '_core_', function (logger) {
+    
+    var webkit = /(webkit)\/([\w.]+)/,  
+        opera = /(opera)(?:.*version)?[ \/]([\w.]+)/,  
+        msie = /(msie) ([\w.]+)/,  
+        mozilla = /(mozilla)(?:.*? rv:([\w.]+))?/,      
+        browser = {},  
+        ua = window.navigator.userAgent, 
+        uaMatch = function (ua) {
+            ua = ua.toLowerCase();
+            
+            var match = webkit.exec(ua) 
+                    || opera.exec(ua) 
+                    || msie.exec(ua) 
+                    || ua.indexOf('compatible') < 0 && mozilla.exec(ua) 
+                    || [];
+            
+            return {
+                browser : match[1] || '',  
+                version : match[2] || '0'
+            };
+        }, 
+        browserMatch = uaMatch(ua);
+    
+    
+    if (browserMatch.browser) {
+        browser[browserMatch.browser] = true;
+        browser.version = browserMatch.version;
+    }
+    return browser;
+});
+'use strict';
+/**
  * 事件系统
  */
 wf.define('Action', '_core_', function (logger) {
