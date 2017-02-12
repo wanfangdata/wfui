33b6a8d212bcb66244d6fd9d2d2deff70f9cd554
bishanshan <skdtb@163.com>
2017-02-12 17:00:26 +0800

    add modal

---
+++ WFUI/public/src/components/modal/modal.js
@@ -0,0 +1,90 @@
+﻿'use strict';
+
+/**
+ * html结构：
+ * <div class="wf-modal wf-modal-info">
+ *     <span class="wf-modal-title">INFO : </span>
+ *     <span class="wf-modal-message">This modal needs your attention, but it’s not important.</span>
+ *     <i class="wf-icon icon-close"></i>
+ * </div>
+ */
+wf.define('UI.Modal', ['UI', 'logger', 'Action'], function (UI, logger, Action) {
+    
+    var role = 'modal';
+    
+    /**
+     * @class Modal
+     */
+    var Modal = wf.inherit(UI, {
+        
+        /**
+         * [data-role]
+         */
+        role: role,
+        
+        /**
+         * 注册用户自定义事件
+         * @event on
+         * @param {String} name 事件名称
+         * @param {Function} func 事件函数
+         */
+        on: function (name, func) {
+            if (!this.action[name]) {
+                logger.error('modal 无{0}事件'.format(name));
+            } else {
+                this.action[name].register(func);
+            }
+        },
+        
+        /**
+         * 关闭警告框
+         */
+        close: function () {
+            this.$element.remove();
+        },
+        
+        /**
+         * ui初始化
+         * @param {String} _base_ 父类同名方法
+         * @param {String} name ui名
+         * @param {Object} $element ui jquery对象
+         * events:{'click',function($element){}}
+         */
+        init: function (_base_, name, $element, events) {
+            var me = this;
+            _base_($element, name);
+            //初始化组件元素,为JQuery对象
+            me.initElement([
+                { selector: 'icon-close' }
+            ]);
+            //初始化事件
+            me.action = {
+                click: new Action('click', function () {
+                    var _action_ = this;
+                    _action_.$target.click(function () {
+                        _action_.piping();
+                    });
+                }, this['icon-close'].$element)
+            };
+            me.initEvent(events);
+        }
+    });
+    
+    /**
+     * dataRole
+     */
+    var dataRole = '[data-role="' + role + '"]';
+    
+    /**
+     * 自动初始化
+     * @param {Object} page页面容器
+     */
+    Modal.auto = function (page) {
+        $.each($(dataRole), function (index) {
+            page.addElement(new Modal('modal' + index, $(this)));
+        });
+    };
+    
+    return Modal;
+
+});
